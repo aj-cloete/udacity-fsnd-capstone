d92e8812e24373ee403c8dbab3fdc6b1bd0d2133
ajcloete <andre.cloete@anmut.co.uk>
2021-03-22 11:54:18 +0200

    Improve how README.md is displayed in HTML

--- common/resources.py
+++ common/resources.py
@@ -2,7 +2,9 @@ from flask import current_app as _app
 from flask import render_template_string
 from flask_restful import Api as _Api
 from flask_restful import Resource
+from gfm import SemiSaneListExtension
 from markdown import markdown
+from mdx_gfm import GithubFlavoredMarkdownExtension
 
 from common.errors import _handle_error
 
@@ -26,5 +28,8 @@ def init_index(app):
     @app.route("/")
     def index():
         with open("README.md", "r") as readme_file:
-            html = markdown(readme_file.read())
+            html = markdown(
+                readme_file.read(),
+                extensions=[GithubFlavoredMarkdownExtension(), SemiSaneListExtension()],
+            )
         return render_template_string(html)
